93e64bcdcab8ac65367936590d3f837de24c97a9
memoor72 <memoor2@yahoo.com>
2023-05-17 20:35:46 +0100

    Created ansible files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Deploy-HA-Application-and-db
 Deploy an application (with high availability) with a database-Using Terraform and Ansible
+Provide database credentials to the application through automation
+ Deploy a second instance of the application, pointing to the same database
+Add a load balancer or reverse proxy to load-balance requests across the two instances
+Store the database credentials in an encrypted form using Ansible Vault
+Add a cron job which takes backups of the database
+Deploy a separate, single instance of the application to a ‘dev’ or ‘test’ environment
--- ec2_instances.tf
+++ ec2_instances.tf
@@ -1,54 +1,77 @@
-data "template_file" "init" {
-   template = file("${path.module}/templates/db-deploy.tmpl")
 
-  vars = {
-    rds_endpoint = aws_db_instance.devpro_rds.address
-    db_user     = var.db_user
-    db_password = var.db_password
+locals {
+  instances = {
+    "nginx-web01"    = "websrvgrp",
+    "tomcat-app01"   = "appsrvgrp",
+    "memcache-mc01"  = "mcsrvgrp",
+    "rabbitmq-rmq01" = "rmqsrvgrp",
+    "mysql-db01"     = "dbsrvgrp",
+
   }
 }
 
-resource "aws_instance" "bastion" {
-  ami                         = var.ami
-  instance_type               = var.instance_type
-  key_name                    = module.key_pair.key_pair_name
-  monitoring                  = true
-  vpc_security_group_ids      = [module.vote_service_sg.security_group_id]
-  subnet_id                   = module.vpc.public_subnets[0]
-  associate_public_ip_address = true
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 4.0"
 
+  for_each = toset(keys(local.instances))
+
+  name = each.key
+
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = module.key_pair.key_pair_name
+  monitoring             = true
+  vpc_security_group_ids = [module.vote_service_sg.security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  associate_public_ip_address = true
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+    Role        = local.instances[each.key]
+  }
+}
+
+data "aws_instance" "created_instances" {
+  for_each = module.ec2_instance
+
+  instance_id = each.value.id
+}
+
+resource "aws_instance" "bastion" {
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = module.key_pair.key_pair_name
+  monitoring             = true
+  vpc_security_group_ids = [module.vote_service_sg.security_group_id]
+  subnet_id              = module.vpc.public_subnets[0]
+
+  associate_public_ip_address = true
   tags = {
     Terraform   = "true"
     Environment = "dev"
     Role        = "bastion"
-
   }
-
-  user_data = data.template_file.init.rendered
-
-  provisioner "file" {
-    content     = templatefile("templates/db-deploy.tmpl", { rds_endpoint = aws_db_instance.devpro_rds.address, db_user = var.db_user, db_password = var.db_password })
-    destination = "/tmp/devpro_dbdeploy.sh"
-  }
-
-
-  provisioner "remote-exec" {
-  inline = [
-    "chmod +x /tmp/devpro_dbdeploy.sh",
-    "sudo /tmp/devpro_dbdeploy.sh 2>&1"
-  ]
-}
-
-
-  connection {
-    user        = var.USERNAME
-    private_key = file(var.PRIV_KEY_PATH)
-    host        = self.public_ip
-  }
-
-  depends_on = [aws_db_instance.devpro_rds]
 }
 
 locals {
   bastion_public_ip = aws_instance.bastion.public_ip
 }
+
+module "inventory_production" {
+  source = "gendall/ansible-inventory/local"
+
+  servers = {
+    for key, value in local.instances :
+    value => [
+      for instance in values(data.aws_instance.created_instances) :
+      instance.public_ip if instance.tags["Role"] == value
+    ]
+  }
+  output = "inventory/production"
+}
+
+
+
+
