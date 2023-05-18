138051c92aa89437e79e419ee69efe4a1601a3aa
memoor72 <memoor2@yahoo.com>
2023-05-18 18:13:50 +0100

    updated backend-services.tf

--- backend-services.tf
+++ backend-services.tf
@@ -55,14 +55,14 @@ resource "aws_lb_listener" "wordpress" {
 
 resource "aws_lb_target_group_attachment" "wordpress1" {
   target_group_arn = aws_lb_target_group.wordpress.arn
-  target_id        = module.ec2_instance["1"].id
+  target_id        = module.ec2_instance["0"].id
 
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "wordpress2" {
   target_group_arn = aws_lb_target_group.wordpress.arn
-  target_id        = module.ec2_instance["2"].id
+  target_id        = module.ec2_instance["1"].id
   port             = 80
 }
 
