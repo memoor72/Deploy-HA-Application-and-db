1626c4a124a2271ca83651516d2ed314168e7b0a
memoor72 <memoor2@yahoo.com>
2023-05-19 09:47:03 +0100

    updated create_key-pair.tf

--- create_key-pair.tf
+++ create_key-pair.tf
@@ -1,11 +1,32 @@
 
-module "key_pair" {
-  source     = "terraform-aws-modules/key-pair/aws"
-  key_name   = "devpro-key"
-  public_key = file(var.public_key_location)
+#module "key_pair" {
+  #source     = "terraform-aws-modules/key-pair/aws"
+  #key_name   = "devpro-key"
+ # public_key = file(var.public_key_location)
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+  #tags = {
+  #  Terraform   = "true"
+ #   Environment = "dev"
+ # }
+#}
+
+
+resource "tls_private_key" "webpress" {
+  algorithm = "RSA"
+}
+
+resource "aws_key_pair" "webpress" {
+  key_name   = "webpress-key"
+  public_key = tls_private_key.webpress.public_key_openssh
+}
+
+output "private_key" {
+  description = "The private key data in PEM format"
+  value       = tls_private_key.webpress.private_key_pem
+  sensitive   = true
+}
+
+resource "local_file" "private_key" {
+  sensitive = tls_private_key.webpress.private_key_pem
+  filename          = var.PRIV_KEY_PATH
 }
