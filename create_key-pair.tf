f1806a9afcc948fb0387edf7d8a673f6bf91c98d
memoor72 <memoor2@yahoo.com>
2023-05-20 16:20:51 +0100

    updated create_key-pair

--- create_key-pair.tf
+++ create_key-pair.tf
@@ -1,22 +1,35 @@
-resource "tls_private_key" "wordpress" {
+
+#module "key_pair" {
+  #source     = "terraform-aws-modules/key-pair/aws"
+  #key_name   = "devpro-key"
+ # public_key = file(var.public_key_location)
+
+  #tags = {
+  #  Terraform   = "true"
+ #   Environment = "dev"
+ # }
+#}
+
+
+resource "tls_private_key" "webpress" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "wordpress" {
-  key_name   = "wordpress-key"
-  public_key = tls_private_key.wordpress.public_key_openssh
+resource "aws_key_pair" "webpress" {
+  key_name   = "webpress-key"
+  public_key = tls_private_key.webpress.public_key_openssh
 }
 
 output "private_key" {
   description = "The private key data in PEM format"
-  value       = tls_private_key.wordpress.private_key_pem
+  value       = tls_private_key.webpress.private_key_pem
   sensitive   = true
 }
 
 resource "local_sensitive_file" "private_key" {
-  content  = tls_private_key.wordpress.private_key_pem
-  filename = "/Users/memoor/.ssh/wordpress-key.pem"
+  content  = tls_private_key.webpress.private_key_pem
+  filename = "/Users/memoor/.ssh/webpress-key.pem"
 
 }
 
